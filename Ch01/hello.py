86b34489c95ef7afeffb3f3702f3d78b7cb8b60e
kbm8527 <kbm8527@gmail.com>
2020-06-22 15:58:44 +0900

    2020/06/22 수업예제

---
+++ Ch01/hello.py
@@ -0,0 +1,13 @@
+"""
+    날짜 : 2020/06/22
+    이름 : 김보미
+    내용 : Python HelloWorld 출력하기
+
+"""
+
+print('Hello World!')
+print("Hello Python!")
+
+# 한줄로 출력하기
+print('Hello', end=' ')
+print('Mr.Kim!')
